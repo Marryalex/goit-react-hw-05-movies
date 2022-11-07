eb8a807a16e0202f69ae4ad9bcfe8479f2ff882f
Marryalex <97538192+Marryalex@users.noreply.github.com>
2022-11-07 17:37:00 +0000

    Deploying to gh-pages from @ Marryalex/goit-react-hw-05-movies@2db2d8fb7cae0ee10a0fd63b03448910d2cea37f 🚀

---
+++ static/js/905.9b39c21e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[905],{905:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184);function u(e){var t=e.msg;return(0,n.jsx)("h2",{children:t})}}}]);
+//# sourceMappingURL=905.9b39c21e.chunk.js.map
